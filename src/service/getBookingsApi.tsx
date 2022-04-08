0ac0fd1d55e1696252c589ed2f32a0d266505c13
Anton <94136841+kodareAnton@users.noreply.github.com>
2022-04-08 09:19:52 +0200

    uppdate 2
    mera fix under bookings service

--- src/service/getBookingsApi.tsx
+++ src/service/getBookingsApi.tsx
@@ -1,15 +1,19 @@
 import axios from "axios";
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { BookingId } from "../models/BookingId";
 
 // hämta hem boking id från Api
 
 export function GetBookings(){
+
+    const [BookingIds, setInfo] = useState<BookingId[]>([])
+
     useEffect(() =>{
         axios
-        .get('https://school-restaurant-api.azurewebsites.net/booking/restaurant/624abc41df8a9fb11c3ea8b6')
+        .get<BookingId[]>('https://school-restaurant-api.azurewebsites.net/booking/restaurant/624abc41df8a9fb11c3ea8b6')
         .then(res =>{
             console.log(res.data);
-            res.data
+            setInfo(res.data)
         })
     })
 }
